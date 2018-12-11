37a717dabed933dd600b68703b155b4790bddc59
RafalSpr <rsprega@o2.pl>
2018-12-11 12:20:06 +0100

    Update czyPierwszaPorownanieSzybkosciDzialaniaDwochFunkcji.py

--- czyPierwszaPorownanieSzybkosciDzialaniaDwochFunkcji.py
+++ czyPierwszaPorownanieSzybkosciDzialaniaDwochFunkcji.py
@@ -9,6 +9,8 @@ def pierwszaSzybciej(a):
     '''
     Funkcja sprawdza, czy liczba jest pierwsza przeglądając liczby od 2 do pierwiastka z a
     '''
+    if i <=1: #oczywiście 1 nie jest pierwsza ;-)
+        return False
     i=2
     while (i*i<=a): # lub alternatywnie i<=sqrt(a)
         if (a % i==0): #znalazłem dzielnik, liczba nie jest pierwsza
@@ -20,6 +22,8 @@ def pierwszaWolniej(a):
     '''
     Metoda brute force, czyli przeglądam wszystkie liczby od 2 do a-1
     '''
+    if i <=1: #oczywiście 1 nie jest pierwsza ;-)
+        return False
     for i in range(2,n):
         if n % i ==0: #znalazłem dzielnik, liczba nie jest pierwsza
             return False
